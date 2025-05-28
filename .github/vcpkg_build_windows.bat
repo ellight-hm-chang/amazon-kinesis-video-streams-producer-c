fba58b873970b5a80b4ab1754d496a31b21e1fa4
ellight.hm.chang <ellight.hm.chang@vivotek.com>
2025-05-28 15:42:18 +0800

    modify setting for vcpkg

--- .github/vcpkg_build_windows.bat
+++ .github/vcpkg_build_windows.bat
@@ -1,15 +1,13 @@
 call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" x86_amd64
+set VCPKG_DEFAULT_TRIPLET=x64-windows
 mkdir build
 cd build
-cmd.exe /c cmake -G "Visual Studio 17 2022" ..
 cmake -G "Visual Studio 17 2022" ^
 -DBUILD_TEST=TRUE ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_TOOLCHAIN_FILE="C:/vcpkg/scripts/buildsystems/vcpkg.cmake" ^
--DCMAKE_PREFIX_PATH="C:/vcpkg/installed/x64-windows" ^
--DCMAKE_INSTALL_PREFIX="C:/Source/kvs_supergiftpack" ^
--DGTest_DIR="C:/vcpkg/installed/x64-windows/share/gtest" ^
--DPKG_CONFIG_EXECUTABLE="C:/vcpkg/packages/pkgconf_x64-windows/tools/pkgconf/pkgconf.exe" ^
+-DCMAKE_GENERATOR_PLATFORM=x64 ^
+-DCMAKE_INSTALL_PREFIX="C:/Source/kvs_supergiftpack/producer" ^
 -DBUILD_COMMON_LWS=TRUE ^
 -DBUILD_COMMON_CURL=TRUE ^
 ..
